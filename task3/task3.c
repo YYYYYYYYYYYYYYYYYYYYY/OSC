1b494082820bd230039c87bb33d7213bad7ef4f0
YYYYYYYYYYYYYYYYYYYYY <45619622+YYYYYYYYYYYYYYYYYYYYY@users.noreply.github.com>
2019-12-14 18:11:57 +0700

    Create task3.c

---
+++ task3/task3.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main ()
+{
+        printf("REAL UID: %d\n", getuid());
+        printf("Effective UID: %d\n", geteuid());
+
+        FILE *f = fopen("./file.txt", "w+");
+        if (!f) perror("SORRY NO\n");
+        else {fclose(f); printf("SAS");}
+
+        setuid(geteuid());
+
+        printf("REAL UID: %d\n", getuid());
+        printf("Effective UID: %d\n", geteuid());
+
+        f = fopen("./file.txt", "w+");
+        if (!f) perror("SORRY NO\n");
+        else {fclose(f); printf("SAS");}
+
+        return 0;
+}
+
